6eda432cf2264ec12e66edde95aad977743c6d75
lichangbin008 <lichangbin008@163.com>
2020-07-08 17:02:59 +0800

    增加第二个adapter

--- app/src/main/java/com/lcb/study/vipcourseuitest/recyclerview/RecyclerViewActivity.java
+++ app/src/main/java/com/lcb/study/vipcourseuitest/recyclerview/RecyclerViewActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.lcb.study.vipcourseuitest.R;
@@ -23,7 +24,8 @@ public class RecyclerViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recycler_view);
 
         recyclerView = findViewById(R.id.table);
-        recyclerView.setAdapter(new MyAdapter());
+//        recyclerView.setAdapter(new MyAdapter());
+        recyclerView.setAdapter(new TwoAdapter(5000));
 
     }
 
@@ -70,6 +72,66 @@ public class RecyclerViewActivity extends AppCompatActivity {
         public MyViewHolder(View itemView) {
             super(itemView);
             tv = itemView.findViewById(R.id.text1);
+        }
+    }
+
+    class ImageViewHolder extends ViewHolder{
+        TextView tv;
+        ImageView imageView;
+        public ImageViewHolder(View view) {
+            super(view);
+            tv=(TextView) view.findViewById(R.id.text2);
+            imageView=(ImageView) view.findViewById(R.id.img);
+        }
+    }
+    class TwoAdapter implements RecyclerView.Adapter<ViewHolder> {
+        int count;
+        public TwoAdapter( int count) {
+            this.count = count;
+        }
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = null;
+            if (viewType == 0) {  //根据不同的viewtype,加载不同的布局
+                view = LayoutInflater.from(RecyclerViewActivity.this).inflate(R.layout.item_table1, parent, false);
+                return new MyViewHolder(view);
+            } else {
+                view = LayoutInflater.from(RecyclerViewActivity.this).inflate(R.layout.item_table2, parent, false);
+                return new ImageViewHolder(view);
+            }
+        }
+
+        @Override
+        public ViewHolder onBindViewHolder(ViewHolder viewHodler, int position) {
+            switch (getItemViewType(position)) {
+                case 0:  //不同的布局，做不同的事
+                    MyViewHolder holderOne = (MyViewHolder) viewHodler;
+                    holderOne.tv.setText("码牛教育 布局1 ");
+                    break;
+                case 1:
+                    ImageViewHolder holderTwo = (ImageViewHolder) viewHodler;
+                    holderTwo.tv.setText("码牛教育 布局2 ");
+            }
+
+            return null;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            if (position%2==1) {   //根据你的条件，返回不同的type
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+        @Override
+        public int getItemCount() {
+            return count;
+        }
+
+        @Override
+        public int getHeight(int index) {
+            return 200;
         }
     }
 }
